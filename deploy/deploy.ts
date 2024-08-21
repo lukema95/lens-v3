8c97af183dc55ca0d1077102a819638b37bcb226
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2024-08-20 21:34:16 -0300

    feat: Deploy script improved to add all the factories

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,19 +2,98 @@ import { deployContract } from "./utils";
 
 import { ethers } from "ethers";
 
-// An example of a basic deploy script
-// It will deploy a Greeter contract to selected network
-// as well as verify it on Block Explorer if possible for the network
 export default async function () {
-  const contractArtifactName = "UsernameFactory";
+  // access control (permissionless)
+  const acessControl_artifactName = "PermissionlessAccessControl";
+  const acessControl_args: any[] = [];
 
-  const constructorArguments = [
-    ethers.getAddress("0xF0d751e78325c4596CD585178a1604b34a3263cd"),
-  ];
+  const acessControl = await deployContract(
+    acessControl_artifactName,
+    acessControl_args,
+    {
+      noVerify: false,
+    }
+  );
 
-  await deployContract(contractArtifactName, constructorArguments, {
-    noVerify: false,
-  });
+  const acessControl_address = await acessControl.getAddress();
 
-  // await deployContract("PermissionlessAccessControl", []);
+  console.log(
+    `\n✔ PermissionlessAccessControl deployed at ${acessControl_address}`
+  );
+
+  // username factory
+  const usernameFactory_artifactName = "UsernameFactory";
+  const usernameFactory_args = [acessControl_address];
+
+  const usernameFactory = await deployContract(
+    usernameFactory_artifactName,
+    usernameFactory_args,
+    {
+      noVerify: false,
+    }
+  );
+
+  console.log(
+    `\n✔ UsernameFactory deployed at ${await usernameFactory.getAddress()}`
+  );
+
+  // graph factory
+  const graphFactory_artifactName = "GraphFactory";
+  const graphFactory_args = [acessControl_address];
+
+  const graphFactory = await deployContract(
+    graphFactory_artifactName,
+    graphFactory_args,
+    {
+      noVerify: false,
+    }
+  );
+
+  console.log(
+    `\n✔ GraphFactory deployed at ${await graphFactory.getAddress()}`
+  );
+
+  // feed factory
+  const feedFactory_artifactName = "FeedFactory";
+  const feedFactory_args = [acessControl_address];
+
+  const feedFactory = await deployContract(
+    feedFactory_artifactName,
+    feedFactory_args,
+    {
+      noVerify: false,
+    }
+  );
+
+  console.log(`\n✔ FeedFactory deployed at ${await feedFactory.getAddress()}`);
+
+  // community factory
+  const communityFactory_artifactName = "CommunityFactory";
+  const communityFactory_args = [acessControl_address];
+
+  const communityFactory = await deployContract(
+    communityFactory_artifactName,
+    communityFactory_args,
+    {
+      noVerify: false,
+    }
+  );
+
+  console.log(
+    `\n✔ CommunityFactory deployed at ${await communityFactory.getAddress()}`
+  );
+
+  // app factory
+  const appFactory_artifactName = "AppFactory";
+  const appFactory_args = [acessControl_address];
+
+  const appFactory = await deployContract(
+    appFactory_artifactName,
+    appFactory_args,
+    {
+      noVerify: false,
+    }
+  );
+
+  console.log(`\n✔ AppFactory deployed at ${await appFactory.getAddress()}`);
 }
