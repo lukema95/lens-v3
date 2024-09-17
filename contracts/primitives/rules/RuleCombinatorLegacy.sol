efb435e27d9c013a7a33c304491c61de06f013ab
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2024-09-17 12:12:12 -0300

    misc: Rule Combinator docs before we decide to remove them
    Co-authored-by: Victor Naumik <vicnaum@gmail.com>

--- contracts/primitives/rules/RuleCombinatorLegacy.sol
+++ contracts/primitives/rules/RuleCombinatorLegacy.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.0;
 import {IRule} from "./IRule.sol";
 import {IAccessControl} from "./../access-control/IAccessControl.sol";
 
-abstract contract RuleCombinator is IRule {
+abstract contract RuleCombinatorLegacy is IRule {
     // Custom RuleCombinator's operation events
     // TODO: Decide betweet plain initilize event + inner operation events, or single initialize event with params
     event Lens_RuleCombinator_Initialized();
     // event Lens_RuleCombinator_Initialized(CombinationMode combinationMode, address accessControl, bytes addRulesData);
+    event Lens_RuleCombinator_CombinationModeChanged(CombinationMode combinationMode);
     event Lens_RuleCombinator_AccessControlChanged(address accessControl);
-    event Lens_RuleCombinator_RuleAdded(address indexed contractAddress, bytes ruleData, bytes combinationData); // TODO: Write a standard for combinationData
+    event Lens_RuleCombinator_RuleAdded(address indexed contractAddress, bytes data);
     event Lens_RuleCombinator_RuleRemoved(address indexed contractAddress, bytes data);
     event Lens_RuleCombinator_RuleUpdated(address indexed contractAddress, bytes data);
 
@@ -24,12 +25,18 @@ abstract contract RuleCombinator is IRule {
     uint256 constant CHANGE_RULE_ACCESS_CONTROL_RID = uint256(keccak256("CHANGE_RULE_ACCESS_CONTROL"));
     uint256 constant CONFIGURE_RULE_RID = uint256(keccak256("CONFIGURE_RULE"));
 
+    enum CombinationMode {
+        AND,
+        OR
+    }
+
     // INITIALIZE is just a special operation for (CHANGE_ACCESS_CONTROL + CHANGE_COMBINATION_MODE + ADD)
     enum Operation {
         INITIALIZE, // No Resource ID permission required, the access control is just being provided.
         ADD_RULES, // CONFIGURE_RULE_RID
         REMOVE_RULES, // CONFIGURE_RULE_RID
         UPDATE_RULES, // CONFIGURE_RULE_RID
+        CHANGE_COMBINATION_MODE, // CONFIGURE_RULE_RID
         CHANGE_ACCESS_CONTROL // CHANGE_RULE_ACCESS_CONTROL_RID
 
     }
@@ -38,11 +45,10 @@ abstract contract RuleCombinator is IRule {
         // TODO: We can have Operation here and have one CONFIGURE_RULES operation instead of three ADD/REMOVE/UPDATE
         address contractAddress;
         bytes data;
-        bool isMandatory;
     }
 
-    address[] internal _mandatoryRules;
-    address[] internal _oneOutOfManyRules;
+    address[] internal _rules;
+    CombinationMode internal _combinationMode; // Default is AND mode
 
     function configure(bytes calldata data) external virtual override {
         require(address(this) != IMPLEMENTATION); // Cannot initialize implementation contract
@@ -53,20 +59,27 @@ abstract contract RuleCombinator is IRule {
         } else if (operation == Operation.CHANGE_ACCESS_CONTROL) {
             _changeAccessControl(operationData);
         } else {
-            // `operation is either ADD_RULES, REMOVE_RULES or UPDATE_RULES.
             require(_canConfigure(msg.sender), "RuleCombinator: Access denied");
-            RuleConfiguration[] memory rules = abi.decode(operationData, (RuleConfiguration[]));
-            if (operation == Operation.ADD_RULES) {
-                _addRules(rules);
-            } else if (operation == Operation.REMOVE_RULES) {
-                _removeRules(rules);
-            } else if (operation == Operation.UPDATE_RULES) {
-                _updateRules(rules);
+            if (operation == Operation.CHANGE_COMBINATION_MODE) {
+                _combinationMode = abi.decode(operationData, (CombinationMode));
             } else {
-                revert("RuleCombinator: Invalid operation");
+                RuleConfiguration[] memory rules = abi.decode(operationData, (RuleConfiguration[]));
+                if (operation == Operation.ADD_RULES) {
+                    _addRules(rules);
+                } else if (operation == Operation.REMOVE_RULES) {
+                    _removeRules(rules);
+                } else if (operation == Operation.UPDATE_RULES) {
+                    _updateRules(rules);
+                } else {
+                    revert("RuleCombinator: Invalid operation");
+                }
             }
         }
         emit Lens_RuleConfigured(data);
+    }
+
+    function getCombinationMode() external view returns (CombinationMode) {
+        return _combinationMode;
     }
 
     function getRules() external view returns (address[] memory) {
@@ -85,12 +98,15 @@ abstract contract RuleCombinator is IRule {
         if (_isInitialized()) {
             revert("RuleCombinator: Already initialized");
         }
-        (address accessControl, bytes memory addRulesData) = abi.decode(operationData, (address, bytes));
+        (CombinationMode combinationMode, address accessControl, bytes memory addRulesData) =
+            abi.decode(operationData, (CombinationMode, address, bytes));
         // TODO: We check all with 0, but we could standrize a Resource ID that is used for this address test/check only
         IAccessControl(accessControl).hasAccess(address(0), address(0), 0); // We expect this to not panic.
         _accessControl = IAccessControl(accessControl);
         emit Lens_RuleCombinator_Initialized();
         emit Lens_RuleCombinator_AccessControlChanged(accessControl);
+        _combinationMode = combinationMode;
+        emit Lens_RuleCombinator_CombinationModeChanged(combinationMode);
         if (addRulesData.length > 0) {
             _addRules(abi.decode(addRulesData, (RuleConfiguration[])));
         }
