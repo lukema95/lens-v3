64902743026d819b8228f14663e5e9a7fe6bb3e4
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2024-10-01 23:55:57 -0300

    feat: Deployment script updated and deployment done

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,75 +1,39 @@
-import { deployContract } from "./utils";
-
-import { ethers } from "ethers";
+import { deployContract } from './utils';
 
 export default async function () {
-  // access control (permissionless)
-  const acessControl_artifactName = "PermissionlessAccessControl";
-  const acessControl_args: any[] = [];
-
-  const acessControl = await deployContract(
-    acessControl_artifactName,
-    acessControl_args,
-    {
-      noVerify: false,
-    }
-  );
-
-  const acessControl_address = await acessControl.getAddress();
-
-  console.log(
-    `\n✔ PermissionlessAccessControl deployed at ${acessControl_address}`
-  );
-
   // username factory
-  const usernameFactory_artifactName = "UsernameFactory";
-  const usernameFactory_args = [acessControl_address];
+  const usernameFactory_artifactName = 'UsernameFactory';
+  const usernameFactory_args: any[] = [];
 
-  const usernameFactory = await deployContract(
-    usernameFactory_artifactName,
-    usernameFactory_args,
-    {
-      noVerify: false,
-    }
-  );
+  const usernameFactory = await deployContract(usernameFactory_artifactName, usernameFactory_args, {
+    noVerify: false,
+  });
 
-  console.log(
-    `\n✔ UsernameFactory deployed at ${await usernameFactory.getAddress()}`
-  );
+  console.log(`\n✔ UsernameFactory deployed at ${await usernameFactory.getAddress()}`);
 
   // graph factory
-  const graphFactory_artifactName = "GraphFactory";
-  const graphFactory_args = [acessControl_address];
+  const graphFactory_artifactName = 'GraphFactory';
+  const graphFactory_args: any[] = [];
 
-  const graphFactory = await deployContract(
-    graphFactory_artifactName,
-    graphFactory_args,
-    {
-      noVerify: false,
-    }
-  );
+  const graphFactory = await deployContract(graphFactory_artifactName, graphFactory_args, {
+    noVerify: false,
+  });
 
-  console.log(
-    `\n✔ GraphFactory deployed at ${await graphFactory.getAddress()}`
-  );
+  console.log(`\n✔ GraphFactory deployed at ${await graphFactory.getAddress()}`);
 
   // feed factory
-  const feedFactory_artifactName = "FeedFactory";
-  const feedFactory_args = [acessControl_address];
+  const feedFactory_artifactName = 'FeedFactory';
+  const feedFactory_args: any[] = [];
 
-  const feedFactory = await deployContract(
-    feedFactory_artifactName,
-    feedFactory_args,
-    {
-      noVerify: false,
-    }
-  );
+  const feedFactory = await deployContract(feedFactory_artifactName, feedFactory_args, {
+    noVerify: false,
+  });
 
   console.log(`\n✔ FeedFactory deployed at ${await feedFactory.getAddress()}`);
 
   // community factory
-  const communityFactory_artifactName = "CommunityFactory";
-  const communityFactory_args = [acessControl_address];
+  const communityFactory_artifactName = 'CommunityFactory';
+  const communityFactory_args: any[] = [];
 
   const communityFactory = await deployContract(
     communityFactory_artifactName,
@@ -79,21 +43,31 @@ export default async function () {
     }
   );
 
-  console.log(
-    `\n✔ CommunityFactory deployed at ${await communityFactory.getAddress()}`
-  );
+  console.log(`\n✔ CommunityFactory deployed at ${await communityFactory.getAddress()}`);
 
   // app factory
-  const appFactory_artifactName = "AppFactory";
-  const appFactory_args = [acessControl_address];
+  const appFactory_artifactName = 'AppFactory';
+  const appFactory_args: any[] = [];
 
-  const appFactory = await deployContract(
-    appFactory_artifactName,
-    appFactory_args,
-    {
-      noVerify: false,
-    }
-  );
+  const appFactory = await deployContract(appFactory_artifactName, appFactory_args, {
+    noVerify: false,
+  });
 
   console.log(`\n✔ AppFactory deployed at ${await appFactory.getAddress()}`);
+
+  // lens factory
+  const lensFactory_artifactName = 'LensFactory';
+  const lensFactory_args = [
+    await appFactory.getAddress(),
+    await communityFactory.getAddress(),
+    await feedFactory.getAddress(),
+    await graphFactory.getAddress(),
+    await usernameFactory.getAddress(),
+  ];
+
+  const lensFactory = await deployContract(lensFactory_artifactName, lensFactory_args, {
+    noVerify: false,
+  });
+
+  console.log(`\n✔ LensFactory deployed at ${await lensFactory.getAddress()}`);
 }
