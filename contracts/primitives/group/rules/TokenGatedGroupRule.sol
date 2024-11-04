6501156f38f80271767a89c1d4d5c2ecac8741b8
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2024-11-04 18:54:29 -0300

    feat: TokenGatedGroupRule added

---
+++ contracts/primitives/group/rules/TokenGatedGroupRule.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IGroupRule} from "../IGroupRule.sol";
+import {TokenGatedRule} from "../../base/TokenGatedRule.sol";
+
+contract TokenGatedGroupRule is TokenGatedRule, IGroupRule {
+    mapping(address group => TokenGateConfiguration configuration) internal _configuration;
+
+    function configure(bytes calldata data) external {
+        TokenGateConfiguration memory configuration = abi.decode(data, (TokenGateConfiguration));
+        _validateTokenGateConfiguration(configuration);
+        _configuration[msg.sender] = configuration;
+    }
+
+    function processJoining(address account, uint256, /* membershipId */ bytes calldata /* data */ )
+        external
+        view
+        returns (bool)
+    {
+        _validateTokenBalance(_configuration[msg.sender], account);
+        return true;
+    }
+
+    function processRemoval(address, /* account */ uint256, /* membershipId */ bytes calldata /*data*/ )
+        external
+        pure
+        returns (bool)
+    {
+        return false;
+    }
+
+    function processLeaving(address, /* account */ uint256, /* membershipId */ bytes calldata /*data*/ )
+        external
+        pure
+        returns (bool)
+    {
+        return false;
+    }
+}
